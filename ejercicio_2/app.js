ccfa94c4688182cbd7709fa99db2d419dc460106
maxip <maxip@DESKTOP-L6MO8I4>
2020-09-18 14:20:27 -0300

    ejercicio2

--- ejercicio_2/app.js
+++ ejercicio_2/app.js
@@ -2,4 +2,9 @@
 const obtenerChiste = require("./library");
 
 // Usar la funcion obtenerChiste() la cual devuelve la promesa de traer el objeto chiste extraido
-obtenerChiste();
+
+obtenerChiste().then( function (x) { 
+    console.log(x[0].setup + " " +x[0].punchline );
+}).catch(function (e){
+    console.log("Error " + e.message);
+});
