407408a0d3687814d136ddae56da3bc3ba6be877
maxip <maxip@DESKTOP-L6MO8I4>
2020-09-18 15:10:18 -0300

    ejercicio3

--- ejercicio_3/app.js
+++ ejercicio_3/app.js
@@ -3,7 +3,14 @@ const obtenerPokemon = require("./library");
 
 // Codigo funcion callback
 
+function estadisticasPokemon(x){
+//console.log(x);
+//console.log(x.stats);   
+console.log("Nombre " + x.name);
+console.log("Stats: ");
+x.stats.forEach(estadistica => {console.log("-"+ estadistica.base_stat + " " + estadistica.stat.name)});
 
+}
 
 // Fin codigo
 
@@ -11,4 +18,4 @@ const obtenerPokemon = require("./library");
 // - nombrePokemon es un string con el nombre del pokemon
 // - funcionCallback es una función que recibe el objeto con los datos del pokemon extraido
 
-obtenerPokemon();
+obtenerPokemon("charizard", estadisticasPokemon);
