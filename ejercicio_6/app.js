bcdd3aeec1f445182341b808d01194fffa7eeb91
maxip <maxip@DESKTOP-L6MO8I4>
2020-09-18 15:40:12 -0300

    ejercicio6

--- ejercicio_6/app.js
+++ ejercicio_6/app.js
@@ -2,4 +2,26 @@
 const obtenerPersonaFake = require("./library");
 
 // Usar la funcion obtenerPersonaFake() la cual devuelve la promesa de traer el objeto persona extraido
-obtenerPersonaFake().then(x => console.log(x));
+obtenerPersonaFake().then(function (x){
+
+        /* console.log(x);
+         console.log(x.results);
+        */
+       x.results.forEach(persona => {
+     
+         console.log("Nombre: " + persona.name.first + " " + persona.name.last);
+         console.log("Sexo: " + persona.gender);
+         console.log("Pais: " + persona.location.country);
+         console.log("Estado: " + persona.location.state);
+         console.log("Calle: " + persona.location.street.name);
+         console.log("Numero: " + persona.location.street.number);
+         console.log("CP: " + persona.location.postcode);
+         console.log("Email: " + persona.email);
+         console.log("Usuario: " + persona.login.username);
+         console.log("Contraseña: " + persona.login.password);
+     
+     })
+
+}).catch(function (err){
+    console.log("Error :C " + err.message);
+});
