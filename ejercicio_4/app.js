5452530735a27d27a70bf6ee81640c9c528cb915
maxip <maxip@DESKTOP-L6MO8I4>
2020-09-18 15:21:35 -0300

    ejercicio4

--- ejercicio_4/app.js
+++ ejercicio_4/app.js
@@ -2,4 +2,12 @@
 const obtenerPokemon = require("./library");
 
 // Usar la funcion obtenerChiste(nombrePokemon) la cual recibe como string un nombre de pokemon y devuelve la promesa de traer el objeto pokemon extraido
-obtenerPokemon();
+obtenerPokemon("dragonite").then(function estadisticaPokeon(x){
+    console.log("Nombre : " + x.name);
+    console.log("Stats:");
+    x.stats.forEach(estadistica => {console.log("-"+ estadistica.base_stat + " " + estadistica.stat.name)});
+
+}).catch(function (err){
+console.log("Error :C " + err.message);
+});  
+
